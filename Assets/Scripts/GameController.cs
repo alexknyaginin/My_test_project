75f08ed981800b198d1089388ca495bfd954f09d
Aleksey <30987602+alexknyaginin@users.noreply.github.com>
2024-05-31 00:15:19 +0300

    добавил подсказки

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -26,6 +27,7 @@ public class GameController : MonoBehaviour
     [SerializeField] private Button _tipOne;
     [SerializeField] private Button _tipTwo;
     [SerializeField] private Button _tipThree;
+    [Range(0, 100)][SerializeField] private byte _callPercent = 60;
 
     [Header("Questions")]
     [SerializeField] private Question[] _questions;
@@ -66,6 +68,7 @@ public class GameController : MonoBehaviour
                     break;
             }
             _ansButtonsText[i].text = text;
+            _ansButtons[i].gameObject.SetActive(true);
         }
     }
     private void EndGame()
@@ -77,6 +80,7 @@ public class GameController : MonoBehaviour
         }
         _tipOne.gameObject.SetActive(false);
         _tipTwo.GetComponentInChildren<TMP_Text>().text = "GO!!!";
+        _tipTwo.enabled = true;
         _tipThree.gameObject.SetActive(false);
         _tipTwo.onClick.RemoveAllListeners();
         _tipTwo.onClick.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
@@ -101,14 +105,87 @@ public class GameController : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
+
+    private int GetRandomInt(int left, int right)
+    {
+        var ms = DateTime.Now.Millisecond;
+        var rand = new System.Random(ms);
+        return rand.Next(left, right);
+    }
     private void Start()
     {
         SetQuestion();
 
-        for(byte i=0; i < _ansButtons.Length; i++)
+        for(byte i = 0; i < _ansButtons.Length; i++)
         {
             var index = i;
             _ansButtons[i].onClick.AddListener(() => OnButtonClick(index));
         }
+        _tipOne.onClick.AddListener(() => 
+        {
+            var buttonsList = new List<Button>();
+
+            var correctIndex = _questions[_currentIndex].CorrectIndex;
+            for (var i = 0; i < _ansButtons.Length; i++)
+            {
+                if (i == correctIndex)
+                {
+                    continue;
+                }
+                buttonsList.Add(_ansButtons[i]);
+            }
+            var randomNumber = GetRandomInt(0, buttonsList.Count);
+            buttonsList.Remove(buttonsList[randomNumber]);
+            buttonsList.ForEach((action) => action.gameObject.SetActive(false));
+            _tipOne.enabled = false;
+        });
+        _tipTwo.onClick.AddListener(() => 
+        {
+            var randomNumber = GetRandomInt(0, 100);
+            var correctIndex = _questions[_currentIndex].CorrectIndex;
+            var answerInt = randomNumber <= _callPercent? correctIndex : GetRandomInt(0, 3);
+            var answerStr = "";
+            switch (answerInt)
+            {
+                case 0 : answerStr = "A";
+                    break;
+                case 1 : answerStr = "B";
+                    break;
+                case 2 : answerStr = "C";
+                    break;
+                case 3 : answerStr = "D";
+                    break;
+            }
+            _qText.text = $"Думаю это {answerStr}";
+            _tipTwo.enabled = false;
+        });
+        _tipThree.onClick.AddListener(() => 
+        {
+            var a = 0;
+            var b = 0;
+            var c = 0;
+            var d = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                var randomNumber = GetRandomInt(0,3);
+                switch (randomNumber)
+                {
+                    case 0:
+                        a++;
+                        break;
+                    case 1:
+                        b++;
+                        break;
+                    case 2:
+                        c++;
+                        break;
+                    case 3:
+                        d++;
+                        break;
+                }
+            }
+            _qText.text = $"A:{a} B:{b} C:{c} D:{d}";
+            _tipThree.enabled = false;
+        });
     }
 }
